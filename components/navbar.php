0ac2c1f8c913a9f19a25fe040157a6d0b43dfa82
Вадим Королёв <pydim@mail.ru>
2025-03-27 22:47:14 +0300

    Исправлен баг с доступом к сессии до её инициализации

--- components/navbar.php
+++ components/navbar.php
@@ -5,7 +5,7 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <?php if ($_SESSION["allowed"]) { ?>
+    <?php if (isset($_SESSION["allowed"]) && $_SESSION["allowed"]) { ?>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
